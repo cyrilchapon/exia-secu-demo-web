18ae8e9aca0ffa3a84959bf2fe343c039a3b5602
CHAPON Cyril <cchapon@cesi.fr>
2016-03-10 15:54:52 +0100

    Created new angular app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name exiaSecuDemoWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the exiaSecuDemoWebApp
+ */
+angular.module('exiaSecuDemoWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name exiaSecuDemoWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the exiaSecuDemoWebApp
+ */
+angular.module('exiaSecuDemoWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
